d0b22647bc5aa6ed12097ea6c4af68756529e79a
jacobtt21 <jacobt1206@gmail.com>
2023-12-30 20:00:25 -0300

    lint fix

--- components/shared/error.tsx
+++ components/shared/error.tsx
@@ -6,7 +6,7 @@ export default function Error({ setStep }: { setStep: Function }) {
     <div className="mt-12 sm:w-[50%] p-4 mx-auto">
       <div className="text pb-2">
         <p className="text-lg mt-8">Something went wrong!</p>
-        <p className="text-base mt-2">We're going to take a look at this.</p>
+        <p className="text-base mt-2">We&apos;re going to take a look at this.</p>
         <div className="mt-8">
           <ActionButton onClick={() => setStep(1)}>Try again</ActionButton>
         </div>
