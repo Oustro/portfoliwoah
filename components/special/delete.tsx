be6d7f5a347f3c23c592ce959a93af6ee4a78f70
jacobtt21 <jacobt1206@gmail.com>
2023-12-31 20:49:16 -0300

    prepped to delete account

---
+++ components/special/delete.tsx
@@ -0,0 +1,7 @@
+export default function Delete() {
+  return (
+    <div className="text-sm p-3 inline-block text-white rounded-lg bg-red-400 hover:bg-red-500 transition cursor-pointer">
+      Delete Account
+    </div>
+  )
+}
--- components/special/profile/info.tsx
+++ components/special/profile/info.tsx
@@ -1,4 +1,5 @@
 import ActionButton from "@/components/shared/actionButton"
+import Delete from "@/components/special/delete"
 
 export default function Info({ setView, name, email, employer }: { setView: Function, name: string, email: string, employer: string }) {
   return (
@@ -32,10 +33,11 @@ export default function Info({ setView, name, email, employer }: { setView: Func
       </div>
       <div className="bg-red-400 text-white pt-4 pb-6 px-4 mt-8 rounded-t-xl">
         <p className="text-xl font-bold">Delete Account</p>
-        <p className="mt-3 text-sm">All data associated with this account will be deleted. This action cannot be undone.</p>
+        <p className="mt-3 text-sm">This action cannot be undone.</p>
       </div>
-      <div className="bg-white border p-5 rounded-b-xl">
-        <p className="text-lg">{email}</p>
+      <div className="bg-white border flex justify-between items-center p-5 rounded-b-xl">
+        <p className="hidden sm:block text-sm">All data associated with this account will be deleted.</p>
+        <Delete />
       </div>
 
     </div>
