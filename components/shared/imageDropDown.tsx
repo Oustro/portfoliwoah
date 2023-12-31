f615a25202a479539e5911125edee84d3c1389e8
jacobtt21 <jacobt1206@gmail.com>
2023-12-30 22:25:44 -0300

    added more styling to nav stuff

---
+++ components/shared/imageDropDown.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState, useEffect, useRef } from "react"
+
+import Image from "next/image"
+
+export default function ImageDropDown({ children, imageSrc, altDescription }: { children: React.ReactNode, imageSrc: string, altDescription: string }) {
+  const [dropdown, setDropdown] = useState(false)
+
+  const ref = useRef<HTMLElement>();
+
+  useEffect(() => {
+    function handleOutsideClick(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        if (setDropdown) {
+          setDropdown(false)
+        }
+      }
+    }
+ 
+    document.addEventListener("click", handleOutsideClick);
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, [ref]);
+  
+  return (
+    <div onClick={() => setDropdown(o => !o)}className="border rounded-full transition">
+      <Image
+      src={imageSrc}
+      alt={altDescription}
+      width={40}
+      height={40}
+      className="rounded-full cursor-pointer hover:bg-slate-300 p-0.5"
+      />
+      {dropdown && (
+        <div className="rounded-lg fixed mt-2 w-64 p-4 bg-white border -ml-48">
+          {children}
+        </div>
+      )}
+    </div>
+  )
+}
--- components/special/logout.tsx
+++ components/special/logout.tsx
@@ -2,10 +2,8 @@
 
 import { signOut } from "next-auth/react"
 
-import ActionButton from "@/components/shared/actionButton"
-
 export default function LogOut() {
   return (
-    <ActionButton onClick={() => signOut({callbackUrl: `${window.location.origin}/register/login`})}>Log Out</ActionButton>
+    <div className="py-2 bg-slate-200 hover:bg-slate-300 text-sm rounded-lg mt-1 text-center transition cursor-pointer" onClick={() => signOut({callbackUrl: `${window.location.origin}/register/login`})}>Log Out</div>
   )
 }
