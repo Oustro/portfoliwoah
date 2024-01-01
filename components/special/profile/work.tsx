0b33c60b0b4dd7d0d3df8443fc139d0311cdb060
jacobtt21 <jacobt1206@gmail.com>
2024-01-01 15:21:09 -0300

    get user posts

--- components/special/profile/work.tsx
+++ components/special/profile/work.tsx
@@ -1,7 +1,31 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
 export default function Work({ font, name, email, employer }: { font: string, name: string, email: string, employer: string }) {
+
+  const [work, setWork] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getWork = async () => {
+      const res = await fetch(`/api/user/work?email=${email}`)
+      const data = await res.json()
+      setWork(data)
+    }
+
+    getWork()
+    setLoading(false)
+
+  }, [])
+
   return (
-    <>
-    hi Work
-    </>
+    <div className="mb-16">
+      {loading ? (
+        <p>Loading...</p> 
+      ) : (
+        <>hi</>
+      )}
+    </div>
   )
 }
