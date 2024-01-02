b240cab910c8f50a140b7fab3a4fc0203fa2e07e
jacobtt21 <jacobt1206@gmail.com>
2024-01-02 20:11:31 -0300

    prepping the preview page

--- components/special/add/preview.tsx
+++ components/special/add/preview.tsx
@@ -1,11 +1,19 @@
+"use client"
+
+import { useState } from "react"
+
 import { projectData } from "@/lib/types"
 import Image from "next/image"
 
+import Spinner from "@/components/shared/spinner"
+
 export default function Preview({ setStep, postInfo }: { setStep: Function, postInfo: projectData }) {
-  
+  const [loading, setLoading] = useState(false)
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    setStep(2)
+    setLoading(true)
+    console.log("saving post")
   }
 
 
@@ -20,7 +28,16 @@ export default function Preview({ setStep, postInfo }: { setStep: Function, post
         height={300}
         className="rounded-lg"
         />
-      <button type="submit" className="w-full mt-4 p-4 rounded-lg bg-slate-200 hover:bg-slate-300 transition focus:outline-none">Post Work</button>
+      <button type="submit" disabled={!postInfo.image || loading ? true: false} className="w-full mt-4 p-4 rounded-lg bg-slate-200 hover:bg-slate-300 transition focus:outline-none disabled:bg-slate-300">
+        {!postInfo.image || loading ? (
+          <div className="flex items-center gap-2 justify-center">
+            <Spinner color="slate-800"/>
+            <span>Loading...</span>
+          </div>
+        ) : 
+          "Post Work"
+        }
+      </button>
     </form>
   )
 }
