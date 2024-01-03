4ea708a83e318fe2d324a3faa944e1b0b4c872a2
jacobtt21 <jacobt1206@gmail.com>
2024-01-02 22:52:53 -0300

    card and card skeleton

---
+++ components/shared/cardSkeleton.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import { projectData } from "@/lib/types";
+
+import { HiCursorClick } from "react-icons/hi";
+
+export default function CardSkeleton({ postInfo, name, employer } : { postInfo: projectData, name: string, employer: string }) {
+  return (
+    <div className="group transition-all rounded-lg">
+      <div className="animate-pulse bg-slate-300 rounded-lg">
+        <Image
+        src="https://0xfozowwtae3snn2.public.blob.vercel-storage.com/uploaded_on_1704242422961-q6WMmOEsiMOyZ4yMgil9daLIYt3UR3.jpg"
+        alt="Project Image"
+        width={300}
+        height={500}
+        className="rounded-lg w-full shadow"
+        />
+      </div>
+      <div className="mt-2 px-1 text-sm flex justify-between">
+        <div>
+          <p>{postInfo.name}</p>
+          <p className="text-xs flex items-center gap-1"><HiCursorClick /> {postInfo.clicks}</p>
+        </div>
+        <div className="text-right">
+          <p>{name}</p>
+          <p className="text-xs">@ {employer}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
--- components/special/add/preview.tsx
+++ components/special/add/preview.tsx
@@ -3,11 +3,12 @@
 import { useState } from "react"
 
 import { projectData } from "@/lib/types"
-import Image from "next/image"
 
+import Card from "@/components/shared/card"
+import CardSkeleton from "@/components/shared/cardSkeleton"
 import Spinner from "@/components/shared/spinner"
 
-export default function Preview({ setStep, postInfo }: { setStep: Function, postInfo: projectData }) {
+export default function Preview({ setStep, postInfo, name, employer }: { setStep: Function, postInfo: projectData, name: string, employer: string }) {
   const [loading, setLoading] = useState(false)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -16,23 +17,18 @@ export default function Preview({ setStep, postInfo }: { setStep: Function, post
     console.log("saving post")
   }
 
-
   return (
     <form onSubmit={(e) => handleSubmit(e)} className="mt-12 sm:w-[50%] p-4 mx-auto">
-      {postInfo.name}
-
-      <Image
-        src={postInfo.image}
-        alt="Post Image"
-        width={500}
-        height={300}
-        className="rounded-lg"
-        />
+      {postInfo.image ? (
+        <Card postInfo={postInfo} name={name} employer={employer} />
+      ) : (
+        <CardSkeleton postInfo={postInfo} name={name} employer={employer} />
+      )}
       <button type="submit" disabled={!postInfo.image || loading ? true: false} className="w-full mt-4 p-4 rounded-lg bg-slate-200 hover:bg-slate-300 transition focus:outline-none disabled:bg-slate-300">
         {!postInfo.image || loading ? (
           <div className="flex items-center gap-2 justify-center">
             <Spinner color="slate-800"/>
-            <span>Loading...</span>
+            <span>Generating Image...</span>
           </div>
         ) : 
           "Post Work"
