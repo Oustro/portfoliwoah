98993053a3c38fa823d6340ab395dfeceffe7f8a
jacobtt21 <jacobt1206@gmail.com>
2024-01-02 23:04:49 -0300

    fixed UX error

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -12,11 +12,11 @@ export default function Card({ postInfo, name, employer } : { postInfo: projectD
         <Image
         src={postInfo.image}
         alt="Project Image"
-        width={300}
-        height={500}
+        width={640}
+        height={360}
         className="rounded-lg w-full border"
         />
-        <div className="opacity-0 group-hover:opacity-100 transition relative bg-gradient-to-t z-20 from-gray-600 from-7% rounded-b-xl h-24 -mt-24 pt-10 px-4">
+        <div className="opacity-0 group-hover:opacity-100 transition-all relative bg-gradient-to-t z-20 from-gray-600 from-7% rounded-b-xl h-24 -mt-24 pt-10 px-4">
           <p className="text-sm text-white">{postInfo.description || "No description."}</p>
         </div>
         <div className="mt-2 px-1 text-sm flex justify-between">
--- components/shared/cardSkeleton.tsx
+++ components/shared/cardSkeleton.tsx
@@ -7,13 +7,13 @@ import { HiCursorClick } from "react-icons/hi";
 export default function CardSkeleton({ postInfo, name, employer } : { postInfo: projectData, name: string, employer: string }) {
   return (
     <div className="group transition-all rounded-lg">
-      <div className="animate-pulse bg-slate-300 rounded-lg">
+      <div className="animate-pulse bg-slate-200 rounded-lg">
         <Image
         src="https://0xfozowwtae3snn2.public.blob.vercel-storage.com/uploaded_on_1704242422961-q6WMmOEsiMOyZ4yMgil9daLIYt3UR3.jpg"
         alt="Project Image"
-        width={300}
-        height={500}
-        className="rounded-lg w-full shadow"
+        width={640}
+        height={360}
+        className="rounded-lg opacity-0 w-full shadow"
         />
       </div>
       <div className="mt-2 px-1 text-sm flex justify-between">
