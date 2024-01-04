19dd513a98f230f4ff3fdbbc82c98837e88eb7bd
jacobtt21 <jacobt1206@gmail.com>
2024-01-04 13:59:23 -0300

    added employer to the mix algo

--- app/api/posts/ranked/route.ts
+++ app/api/posts/ranked/route.ts
@@ -1,18 +1,26 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 import prisma from '@/utils/db'
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+
+  const employer = request.nextUrl.searchParams.get('employer');
 
   try {
 
     const alpha = 0.1; 
 
+    let whereClause = '';
+    if (employer) {
+      whereClause = `WHERE u."employer" = ${employer}`;
+    }
+
     const posts = await prisma.$queryRaw`
       SELECT p.*, 
-            u.*,
-            p."clicks" / (1 + ${alpha} * EXTRACT(EPOCH FROM (NOW() - p."created") / 3600)) as "score"
+             u.*,
+             p."clicks" / (1 + ${alpha} * EXTRACT(EPOCH FROM (NOW() - p."created") / 3600)) as "score"
       FROM "Project" as p
       LEFT JOIN "UserInfo" as u ON p."userEmail" = u."email"
+      ${whereClause}
       ORDER BY "score" DESC
     `;
 
