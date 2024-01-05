265ea276303c03539e3f8a435bb987e7d8c85a66
jacobtt21 <jacobt1206@gmail.com>
2024-01-04 21:51:28 -0300

    changed from full name to display name

--- components/special/signup/name.tsx
+++ components/special/signup/name.tsx
@@ -8,11 +8,11 @@ export default function Name({ setStep, setUserInfo, userInfo }: { setStep: Func
   return (
     <form onSubmit={(e) => handleSubmit(e)} className="mt-12 sm:w-[50%] p-4 mx-auto">
       <div className="text-left text-sm pb-2">
-        <label><span className="text-red-600">*</span> Full Name</label>
+        <label><span className="text-red-600">*</span> Display Name</label>
       </div>
       <input
       className="w-full pl-2 py-4 rounded-lg border-2 border-slate-200 focus:outline-none focus:border-slate-300 transition"
-      placeholder="Andy Warhol"
+      placeholder="Andy Warhol, Banksy, etc."
       onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
       required
       />
