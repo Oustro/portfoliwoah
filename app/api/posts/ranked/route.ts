f6ed1f7f81fc3e3c5ef4fe889b81c1620fb4c374
jacobtt21 <jacobt1206@gmail.com>
2024-01-07 14:15:21 -0300

    added a search to find the employers

--- app/api/posts/ranked/route.ts
+++ app/api/posts/ranked/route.ts
@@ -1,22 +1,29 @@
 import { NextResponse, NextRequest } from 'next/server'
 import prisma from '@/utils/db'
+import { Prisma } from '@prisma/client';
 
 import { cardData } from '@/lib/types';
 
 export async function GET(request: NextRequest) {
 
-  const employer = request.nextUrl.searchParams.get('employer');
+  const employer = request.nextUrl.searchParams.get('employer')
 
   try {
 
     const alpha = 0.1
 
+    let additionalWhereClause = '';
+    if (employer) {
+      additionalWhereClause = `WHERE LOWER(u."employer") = LOWER('${employer.replace(/'/g, "''")}')`;
+    }
+    
     const posts = await prisma.$queryRaw`
       SELECT p.*, 
             u.*,
             p."clicks" / (1 + ${alpha} * EXTRACT(EPOCH FROM (NOW() - p."created") / 3600)) as "score"
       FROM "Project" as p
       LEFT JOIN "UserInfo" as u ON p."userEmail" = u."email"
+      ${Prisma.sql([additionalWhereClause])}
       ORDER BY "score" DESC
     ` as cardData
 
--- components/special/posts/mainCards.tsx
+++ components/special/posts/mainCards.tsx
@@ -8,10 +8,11 @@ import Card from "./card"
 export default function MainCards({ email } : { email: string }) {
 
   const [work, setWork] = useState([])
+  const [employer, setEmployer] = useState('')
   const [loading, setLoading] = useState(true)
 
-  const getWork = async () => {
-    const userWorkReponse = await fetch(`/api/posts/ranked?employer=${''}`)
+  const getWork = async (employer: string) => {
+    const userWorkReponse = await fetch(`/api/posts/ranked?employer=${employer}`)
     const data = await userWorkReponse.json()
 
     setWork(data.posts)
@@ -19,12 +20,27 @@ export default function MainCards({ email } : { email: string }) {
   }
 
   useEffect(() => {
-    getWork()
+    getWork(employer)
 
-  }, [])
+  }, [employer])
 
   return (
     <div className="mb-16">
+      <div className="text-left text-sm pb-2">
+        <label>Employer Search</label>
+      </div>
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+        </div>
+        <input
+        className="block pl-10 w-full py-4 mb-8 rounded-lg border-2 border-slate-200 focus:outline-none focus:border-slate-300 transition"
+        placeholder="Airbnb, Google, Vercel, etc."
+        onChange={(e) => setEmployer(e.target.value)}
+        />
+      </div>
       {loading ? (
         <div className="grid sm:grid-cols-3 gap-4">
           <CardSkeleton />
