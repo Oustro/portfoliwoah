014450b48296b5a331ce245fd4463f731cb5accf
jacobtt21 <jacobt1206@gmail.com>
2024-01-09 18:56:02 -0300

    fixed typo

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
       <div className="mt-20 text-center">
         <BadgeOutline>🥳 Welcome to the newly launched Portfoliwoah 🎉</BadgeOutline>
         <h1 className='mt-8 text-4xl sm:text-5xl'>For the Love of <span className={ibm_plex_serif.className}>Design</span>.</h1>
-        <p className="mt-8 text-xs sm:text-base">Share what your proud of and connect with others who love design.</p>
+        <p className="mt-8 text-xs sm:text-base">Share what you're proud of and connect with others who love design.</p>
         <div className="mt-4 text-center flex justify-center gap-4">
           <Video video="https://www.youtube.com/embed/JLJTnYzyRrg" />
           <LinkButton link={session ? "/add" : "/register/login"}>Add your work</LinkButton>
